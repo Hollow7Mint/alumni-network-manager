f04ab297fb1948a4a22ac1bc3d4372b8fb4e5c66
James Holloway <james.holloway94@gmail.com>
2026-04-28 15:39:29 +0000

    set ssh tunnel sync stamp in config

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-04-28 14:59:11 UTC
+# Last sync: 2026-04-28 15:39:29 UTC
