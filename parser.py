2d2032c39845b0ddb0a7fb921ba5c198dbb5c480
James Holloway <james.holloway94@gmail.com>
2026-04-28 18:40:02 +0000

    preserve ssh certificate revocation sync in metadata

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-04-28 17:09:15 UTC
+# Last sync: 2026-04-28 18:40:02 UTC
