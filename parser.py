7ed972836fb31ac82b41770139f5ede5d5b365e2
James Holloway <james.holloway94@gmail.com>
2026-04-30 11:27:45 +0000

    thread cloudtrail digest sync through config banner

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-04-30 10:41:10 UTC
+# Last sync: 2026-04-30 11:27:45 UTC
