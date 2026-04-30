9ef9e9410f12c43f7130f821b39f2e55270a2890
James Holloway <james.holloway94@gmail.com>
2026-04-30 18:39:03 +0000

    sync ssm parameter refresh after pipeline pass

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-04-30 17:53:04 UTC
+# Last sync: 2026-04-30 18:39:03 UTC
