70bb9ce6b827e54e52959412d8c2168cb28c9ac7
James Holloway <james.holloway94@gmail.com>
2026-05-05 13:06:18 +0000

    write mongodb index rebuild time into metadata

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-05 12:01:11 UTC
+# Last sync: 2026-05-05 13:06:18 UTC
