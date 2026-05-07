80c071e722788ca2e2c41d12cd1987d95e08d952
James Holloway <james.holloway94@gmail.com>
2026-05-07 22:55:36 +0000

    flush ec2 fleet inventory marker after scan

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-07 21:45:10 UTC
+# Last sync: 2026-05-07 22:55:36 UTC
