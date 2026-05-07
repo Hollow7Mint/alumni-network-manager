a56c0b5dc28cd1f92c5b01639438be18ae441a46
James Holloway <james.holloway94@gmail.com>
2026-05-07 00:47:27 +0000

    recheck ecs service sync after deployment window

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-07 00:07:46 UTC
+# Last sync: 2026-05-07 00:47:27 UTC
