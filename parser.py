d417fe91cfa74a921b56d1fb965e5ae6dede76b1
James Holloway <james.holloway94@gmail.com>
2026-05-09 15:54:52 +0000

    surface codebuild log sync time for pipeline audit

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-09 14:30:08 UTC
+# Last sync: 2026-05-09 15:54:52 UTC
