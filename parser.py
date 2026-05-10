c1d49cb16ef931f3a7ef03ff952cc8896ce5274c
James Holloway <james.holloway94@gmail.com>
2026-05-10 19:15:12 +0000

    remap ec2 security group rule time in config

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-10 18:24:21 UTC
+# Last sync: 2026-05-10 19:15:12 UTC
