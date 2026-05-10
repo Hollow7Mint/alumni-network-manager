111430014de52832c7dc67f20afedd8e62242815
James Holloway <james.holloway94@gmail.com>
2026-05-10 17:55:06 +0000

    touch ssh tunnel sync during scheduled refresh

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-10 16:41:48 UTC
+# Last sync: 2026-05-10 17:55:05 UTC
