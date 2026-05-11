01cb733a777cfe460d87a610a59fc5109f88133b
James Holloway <james.holloway94@gmail.com>
2026-05-11 09:41:25 +0000

    snapshot iam role binding time for audit trail

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-11 08:49:11 UTC
+# Last sync: 2026-05-11 09:41:25 UTC
