3c8b5ddc3fc94194f5f5af7b3cdff14600b22c6b
James Holloway <james.holloway94@gmail.com>
2026-05-12 09:42:13 +0000

    harvest ssh fingerprint sync for nightly audit

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-12 08:40:59 UTC
+# Last sync: 2026-05-12 09:42:13 UTC
