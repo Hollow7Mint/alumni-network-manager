80421ab9bb1cbb8c8463003514071e5b1a1bdfdf
James Holloway <james.holloway94@gmail.com>
2026-05-12 16:33:51 +0000

    publish ssh key distribution sync after rotation

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-12 15:55:39 UTC
+# Last sync: 2026-05-12 16:33:51 UTC
