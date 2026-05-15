f7039f57e82f8bb8dcfbfc993949c05b456eeb43
James Holloway <james.holloway94@gmail.com>
2026-05-15 06:44:08 +0000

    reconcile cloudwatch dashboard sync after update

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-15 06:00:38 UTC
+# Last sync: 2026-05-15 06:44:08 UTC
