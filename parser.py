dcd6808ec994e7dcd6b39043f99239042f1ca408
James Holloway <james.holloway94@gmail.com>
2026-05-16 17:37:39 +0000

    adjust bastion acl sync stamp after ip review

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-16 16:26:17 UTC
+# Last sync: 2026-05-16 17:37:39 UTC
