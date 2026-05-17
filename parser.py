184cd1e37c0e2e6f88de1cdd1d654978a8c2db3a
James Holloway <james.holloway94@gmail.com>
2026-05-17 17:51:10 +0000

    write bastion access log time into metadata

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-17 16:38:22 UTC
+# Last sync: 2026-05-17 17:51:10 UTC
