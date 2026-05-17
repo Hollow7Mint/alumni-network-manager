2117cc66ef9061b9cf4292f14e360a6a4a1d8edc
James Holloway <james.holloway94@gmail.com>
2026-05-17 00:16:09 +0000

    merge ssh trust anchor time into metadata

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-16 23:29:08 UTC
+# Last sync: 2026-05-17 00:16:09 UTC
