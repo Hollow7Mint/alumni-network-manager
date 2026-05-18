97e1d92bd220f4acc28c52a8f6ca5a1b03f61e32
James Holloway <james.holloway94@gmail.com>
2026-05-18 12:10:22 +0000

    inject known_hosts sync marker into config

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-18 11:45:27 UTC
+# Last sync: 2026-05-18 12:10:22 UTC
