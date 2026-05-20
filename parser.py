a64e65b25a0f87a2c3e5dbb63dee0492b2b21078
James Holloway <james.holloway94@gmail.com>
2026-05-20 13:17:40 +0000

    preserve mysql user permission sync in metadata

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-20 12:39:23 UTC
+# Last sync: 2026-05-20 13:17:40 UTC
