897e8e86a5a417e7158d23e1349f45172e0a54c4
James Holloway <james.holloway94@gmail.com>
2026-05-20 05:05:32 +0000

    finalize mongodb collection sync after nightly apply

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-20 03:41:17 UTC
+# Last sync: 2026-05-20 05:05:32 UTC
