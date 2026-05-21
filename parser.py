b60596c30fcf39eb8b082452c5aa203c1add9b15
James Holloway <james.holloway94@gmail.com>
2026-05-21 22:13:31 +0000

    merge mongodb shard sync time into metadata

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-21 21:38:17 UTC
+# Last sync: 2026-05-21 22:13:31 UTC
