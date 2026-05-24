aca1a37d752ac4f8c987eac3c51610ef7ecb2c17
James Holloway <james.holloway94@gmail.com>
2026-05-24 02:20:21 +0000

    backfill s3 versioning sync marker in metadata

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-24 01:58:53 UTC
+# Last sync: 2026-05-24 02:20:21 UTC
