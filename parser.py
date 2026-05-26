7357529ac9b83181665129db073dc908220a7936
James Holloway <james.holloway94@gmail.com>
2026-05-26 17:49:08 +0000

    normalize mysql charset sync time in metadata

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-26 16:31:18 UTC
+# Last sync: 2026-05-26 17:49:08 UTC
