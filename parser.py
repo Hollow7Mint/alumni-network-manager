afe8b6c5c3a29a3d1449f2316f74b3ad0be2063e
James Holloway <james.holloway94@gmail.com>
2026-05-28 09:22:43 +0000

    mirror ssh certificate chain time in record

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-28 08:59:03 UTC
+# Last sync: 2026-05-28 09:22:43 UTC
