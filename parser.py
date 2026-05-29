9906f536f5f22dc7bc2414f0698c50cf53769979
James Holloway <james.holloway94@gmail.com>
2026-05-29 08:44:12 +0000

    pin mongodb shard balancing time in config

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-29 07:57:27 UTC
+# Last sync: 2026-05-29 08:44:12 UTC
