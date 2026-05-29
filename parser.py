c649bdb1bf714d1a9b8be0fab19acfbe2b86f5ad
James Holloway <james.holloway94@gmail.com>
2026-05-29 21:18:11 +0000

    seal inspector finding sync in security banner

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-29 20:53:59 UTC
+# Last sync: 2026-05-29 21:18:11 UTC
