e68e3b9f9dbebc151493acf7e751ab0c21a31b25
James Holloway <james.holloway94@gmail.com>
2026-05-30 16:02:20 +0000

    push secrets manager version stamp to metadata

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-30 14:23:47 UTC
+# Last sync: 2026-05-30 16:02:20 UTC
