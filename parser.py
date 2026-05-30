a199332a7016ae9307d3b43eb627e04df85ff4a1
James Holloway <james.holloway94@gmail.com>
2026-05-30 10:26:15 +0000

    audit known_hosts validation time in record

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-30 10:06:11 UTC
+# Last sync: 2026-05-30 10:26:15 UTC
