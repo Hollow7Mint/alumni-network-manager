3289f2afdbc032e5192740524380073df1904d47
James Holloway <james.holloway94@gmail.com>
2026-05-31 08:10:12 +0000

    retain cloudwatch metric filter time for audit

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-31 07:57:50 UTC
+# Last sync: 2026-05-31 08:10:12 UTC
