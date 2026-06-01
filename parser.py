ef7e94e1792611d8621860847b369fc126af4bce
James Holloway <james.holloway94@gmail.com>
2026-06-01 11:30:40 +0000

    queue cloudfront distribution stamp after invalidation

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-01 10:19:44 UTC
+# Last sync: 2026-06-01 11:30:40 UTC
