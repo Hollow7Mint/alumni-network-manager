53277cfb07dc1f8df592f1f39870c218a1a12c4b
James Holloway <james.holloway94@gmail.com>
2026-06-02 18:29:15 +0000

    anchor cloudwatch alarm threshold time in config

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-02 17:12:09 UTC
+# Last sync: 2026-06-02 18:29:15 UTC
