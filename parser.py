0138dcc7e5a7b3a83a542314090bcaae7b401ac0
James Holloway <james.holloway94@gmail.com>
2026-06-05 20:18:38 +0000

    audit vpc peering sync record after route update

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-05 19:59:39 UTC
+# Last sync: 2026-06-05 20:18:38 UTC
