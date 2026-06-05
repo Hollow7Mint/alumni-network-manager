0c1544c9a403b3bf276d4e3be596e4bbfc949b75
James Holloway <james.holloway94@gmail.com>
2026-06-05 21:29:22 +0000

    restore ssh public key time for rotation audit

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-05 20:18:38 UTC
+# Last sync: 2026-06-05 21:29:22 UTC
