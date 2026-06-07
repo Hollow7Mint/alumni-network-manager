20640352844638d37a62012f70b42bdcf669b6b8
James Holloway <james.holloway94@gmail.com>
2026-06-07 15:24:41 +0000

    advance cdk app deploy time into config

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-07 14:51:35 UTC
+# Last sync: 2026-06-07 15:24:41 UTC
