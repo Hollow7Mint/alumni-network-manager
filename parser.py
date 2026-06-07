c1a93936c4102ed79cd185ea166cab38a114730f
James Holloway <james.holloway94@gmail.com>
2026-06-07 23:49:15 +0000

    flush mongodb replica lag marker for nightly check

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-07 22:42:36 UTC
+# Last sync: 2026-06-07 23:49:15 UTC
