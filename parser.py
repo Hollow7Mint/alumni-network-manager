70fc223d27c4b1b951619d0f008c93665b65e6bf
James Holloway <james.holloway94@gmail.com>
2026-06-09 09:49:13 +0000

    frame mysql connection limit sync in config

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-09 09:00:12 UTC
+# Last sync: 2026-06-09 09:49:13 UTC
