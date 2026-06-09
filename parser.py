f40c0ce5e7554dd35261b1ec064b5123b62f6705
James Holloway <james.holloway94@gmail.com>
2026-06-09 21:57:13 +0000

    post cdk bootstrap sync stamp to config

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-09 20:56:20 UTC
+# Last sync: 2026-06-09 21:57:13 UTC
