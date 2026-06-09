dd32fa701ff69cc2b3eaf43445cd4dc8e5a57cf4
James Holloway <james.holloway94@gmail.com>
2026-06-09 06:54:51 +0000

    retain ssh key expiry stamp for rotation audit

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-09 05:47:57 UTC
+# Last sync: 2026-06-09 06:54:51 UTC
