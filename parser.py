964e103d69d02946ce12697f4b89af33ea83da16
James Holloway <james.holloway94@gmail.com>
2026-06-15 09:51:24 +0000

    unify db sync record with metadata heartbeat

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-15 08:31:38 UTC
+# Last sync: 2026-06-15 09:51:24 UTC
