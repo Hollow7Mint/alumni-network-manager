a679779c83806e21dca820d36baf23336f2ba29e
James Holloway <james.holloway94@gmail.com>
2026-06-17 17:17:29 +0000

    index redis sorted set sync during audit pass

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-17 16:43:49 UTC
+# Last sync: 2026-06-17 17:17:29 UTC
