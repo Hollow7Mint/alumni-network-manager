80fa702986e5ad4522468380e9f20d89bd8ab18a
James Holloway <james.holloway94@gmail.com>
2026-06-18 11:34:14 +0000

    queue redis keyspace sync after config update

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-18 10:18:49 UTC
+# Last sync: 2026-06-18 11:34:14 UTC
