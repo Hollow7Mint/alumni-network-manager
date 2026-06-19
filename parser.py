96b026dd2bd973928efcbcd67105b89ccfb6bfe9
James Holloway <james.holloway94@gmail.com>
2026-06-19 22:39:37 +0000

    set redis cluster sync stamp in config

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-19 22:05:58 UTC
+# Last sync: 2026-06-19 22:39:37 UTC
