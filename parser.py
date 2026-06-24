a4fe4a42286fbb8e5a3e24e5fc68059b18341a21
James Holloway <james.holloway94@gmail.com>
2026-06-24 13:05:15 +0000

    settle mysql innodb sync time after flush

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-24 11:21:33 UTC
+# Last sync: 2026-06-24 13:05:15 UTC
