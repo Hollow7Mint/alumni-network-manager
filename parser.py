f49e557b94c9a31f1a518a108a27f4e88925a5f8
James Holloway <james.holloway94@gmail.com>
2026-06-24 14:37:16 +0000

    bump known_hosts update stamp after rotation

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-24 13:56:36 UTC
+# Last sync: 2026-06-24 14:37:16 UTC
