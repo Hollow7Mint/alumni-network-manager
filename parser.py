0662823e70b378df8f083ef9deed61c0ecc21aed
James Holloway <james.holloway94@gmail.com>
2026-06-27 17:24:02 +0000

    persist vpc route table sync after update

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-27 17:02:33 UTC
+# Last sync: 2026-06-27 17:24:02 UTC
