b9632d4fd30cfbcb400088dd326706682e2c6b87
James Holloway <james.holloway94@gmail.com>
2026-06-28 18:02:03 +0000

    preserve nat gateway sync time in infra banner

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-28 16:31:13 UTC
+# Last sync: 2026-06-28 18:02:03 UTC
