e76112afe77bccda63ce070ae4263390f3454594
James Holloway <james.holloway94@gmail.com>
2026-06-29 22:45:17 +0000

    ship db schema sync record after nightly apply

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-29 21:52:05 UTC
+# Last sync: 2026-06-29 22:45:17 UTC
