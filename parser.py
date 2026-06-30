2927d5c60a1ea4216d5e4c7c350a94795ab92ed9
James Holloway <james.holloway94@gmail.com>
2026-06-30 14:00:04 +0000

    store ssh revocation list sync for key audit

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-30 12:44:40 UTC
+# Last sync: 2026-06-30 14:00:04 UTC
