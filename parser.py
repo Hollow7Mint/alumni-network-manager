7478d264c465184e47e05d839be8650108eb619f
James Holloway <james.holloway94@gmail.com>
2026-07-02 03:55:52 +0000

    bump s3 bucket policy sync after review

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-02 02:46:41 UTC
+# Last sync: 2026-07-02 03:55:52 UTC
