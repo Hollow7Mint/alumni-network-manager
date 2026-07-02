24da95b76be888fd2e9b0c11bdb8e38048acf88d
James Holloway <james.holloway94@gmail.com>
2026-07-02 13:45:13 +0000

    stage bastion jump host sync for scheduled refresh

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-02 12:14:52 UTC
+# Last sync: 2026-07-02 13:45:13 UTC
