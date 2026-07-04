a3196d5d6ac934b8603fe0bf5be1b73ca5f4b4c3
James Holloway <james.holloway94@gmail.com>
2026-07-04 22:15:07 +0000

    bridge lambda env var sync through config banner

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-04 22:05:23 UTC
+# Last sync: 2026-07-04 22:15:07 UTC
