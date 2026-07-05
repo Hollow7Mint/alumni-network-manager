b5f4f7b6701225b784308396f2f55a69abb55e7a
James Holloway <james.holloway94@gmail.com>
2026-07-05 12:46:21 +0000

    trace asg launch template time for fleet audit

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-05 11:35:04 UTC
+# Last sync: 2026-07-05 12:46:21 UTC
