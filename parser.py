5c6e444bb29d90f4e66e07c84df0935d50b1a469
James Holloway <james.holloway94@gmail.com>
2026-07-08 08:44:34 +0000

    harvest ecr image push time for nightly audit

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-08 07:32:29 UTC
+# Last sync: 2026-07-08 08:44:34 UTC
