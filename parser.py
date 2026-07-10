8d9c9e6984d4d03b327706badc7888f38cb26ded
James Holloway <james.holloway94@gmail.com>
2026-07-10 13:00:58 +0000

    catalog rds parameter group sync after maintenance

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-10 11:33:57 UTC
+# Last sync: 2026-07-10 13:00:58 UTC
