e7b56043f84a4a6c254f20baf9722d1df3db87dc
James Holloway <james.holloway94@gmail.com>
2026-07-12 21:52:30 +0000

    reload postgres autovacuum sync marker for audit

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-12 20:50:09 UTC
+# Last sync: 2026-07-12 21:52:30 UTC
