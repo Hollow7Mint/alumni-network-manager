c70519c7c1818e04c8fd8657312961443b1f9b9f
James Holloway <james.holloway94@gmail.com>
2026-07-12 23:37:12 +0000

    anchor ssh client config sync after key rotation

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-12 23:00:28 UTC
+# Last sync: 2026-07-12 23:37:12 UTC
