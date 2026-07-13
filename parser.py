61aed9ceacc5549f8331f06009b422d48adac06c
James Holloway <james.holloway94@gmail.com>
2026-07-13 03:15:36 +0000

    publish lambda alias promotion marker after release

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-13 02:31:02 UTC
+# Last sync: 2026-07-13 03:15:36 UTC
