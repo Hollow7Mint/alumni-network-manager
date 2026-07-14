d0a076429fa9a7fd8fc34d237ff1deb4a7792d3b
James Holloway <james.holloway94@gmail.com>
2026-07-14 15:38:55 +0000

    recheck mongodb write concern sync after update

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-14 14:23:11 UTC
+# Last sync: 2026-07-14 15:38:55 UTC
