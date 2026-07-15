d18e803034cab9bdcea3651c25692643ba106630
James Holloway <james.holloway94@gmail.com>
2026-07-15 22:14:09 +0000

    keep ecs task sync marker fresh after rollout

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-15 21:46:03 UTC
+# Last sync: 2026-07-15 22:14:09 UTC
