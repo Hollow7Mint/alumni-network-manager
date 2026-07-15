1fae14e31495a4843296117659f03d907ed4d08f
James Holloway <james.holloway94@gmail.com>
2026-07-15 17:18:31 +0000

    bridge ssh ProxyJump config sync through banner

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-15 16:44:33 UTC
+# Last sync: 2026-07-15 17:18:31 UTC
