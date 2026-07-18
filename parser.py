5161b0b78deb522ba2a67155e8cf0f35992b1c11
James Holloway <james.holloway94@gmail.com>
2026-07-18 12:28:47 +0000

    save bastion policy sync marker after review

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-18 11:10:49 UTC
+# Last sync: 2026-07-18 12:28:47 UTC
