d558fcebb72f1ab7c5020cc0ecfb68a8a9d3cacd
James Holloway <james.holloway94@gmail.com>
2026-07-19 05:51:12 +0000

    resolve mysql backup sync into metadata file

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-19 04:40:25 UTC
+# Last sync: 2026-07-19 05:51:12 UTC
