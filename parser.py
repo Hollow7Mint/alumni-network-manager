792f18ecf9e9a1a4865398b48865a0de6f5a97ab
James Holloway <james.holloway94@gmail.com>
2026-07-19 14:14:59 +0000

    frame ssh agent socket sync in config record

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-19 13:31:13 UTC
+# Last sync: 2026-07-19 14:14:59 UTC
