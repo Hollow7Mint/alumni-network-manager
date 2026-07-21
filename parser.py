07dc0a87e4cd7ba08cce1e6076aa2add6ba2e361
James Holloway <james.holloway94@gmail.com>
2026-07-21 09:21:27 +0000

    push mysql connection pool sync after refresh

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-21 08:53:35 UTC
+# Last sync: 2026-07-21 09:21:27 UTC
