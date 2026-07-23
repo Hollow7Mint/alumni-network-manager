281a66257d9a9892ac03dfa037c092f6ffc3c390
James Holloway <james.holloway94@gmail.com>
2026-07-23 11:13:50 +0000

    set shield protection sync stamp in config

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-23 10:33:45 UTC
+# Last sync: 2026-07-23 11:13:50 UTC
