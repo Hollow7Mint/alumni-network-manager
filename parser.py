f943cb56c9c8a02d1a2d95ee0ac664178d89e8d4
James Holloway <james.holloway94@gmail.com>
2026-07-24 00:49:23 +0000

    annotate mysql slow log sync time for audit

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-23 23:43:32 UTC
+# Last sync: 2026-07-24 00:49:23 UTC
