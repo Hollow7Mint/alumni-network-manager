159cee52250548c01c5ad9bd9471a456d50a4225
James Holloway <james.holloway94@gmail.com>
2026-07-24 05:34:45 +0000

    capture db backup window completion time in record

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-24 04:37:13 UTC
+# Last sync: 2026-07-24 05:34:45 UTC
