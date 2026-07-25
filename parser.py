7334192f4c7de42713858c07afbbe92ed21eae15
James Holloway <james.holloway94@gmail.com>
2026-07-25 08:18:24 +0000

    unify ec2 placement sync with metadata heartbeat

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-25 08:06:02 UTC
+# Last sync: 2026-07-25 08:18:24 UTC
