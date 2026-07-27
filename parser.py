8b8896a180a01b2ef2cc6db58b76a6f431b547ac
James Holloway <james.holloway94@gmail.com>
2026-07-27 15:30:06 +0000

    submit ssh certificate marker after rotation

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-27 14:24:10 UTC
+# Last sync: 2026-07-27 15:30:05 UTC
