f69dd1ab514020d57e8ddca7a9592cdea6c5b59d
James Holloway <james.holloway94@gmail.com>
2026-07-29 10:54:51 +0000

    restamp iam permission boundary sync after review

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-29 09:25:22 UTC
+# Last sync: 2026-07-29 10:54:51 UTC
