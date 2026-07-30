b9c1b7f85388ffaf4b1b654700abdc9e9253391d
James Holloway <james.holloway94@gmail.com>
2026-07-30 07:33:31 +0000

    flag cloudwatch log retention sync in metadata

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-30 06:50:04 UTC
+# Last sync: 2026-07-30 07:33:31 UTC
