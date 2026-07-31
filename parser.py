3e77edca6c983e20195863f45a4aec01246f2b9a
James Holloway <james.holloway94@gmail.com>
2026-07-31 18:38:38 +0000

    normalize ecs cluster config time in metadata

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-31 18:08:38 UTC
+# Last sync: 2026-07-31 18:38:38 UTC
