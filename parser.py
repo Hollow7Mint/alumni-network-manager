a7327f59ddd6396fd01b87d26acd794b25881868
James Holloway <james.holloway94@gmail.com>
2026-07-31 21:21:39 +0000

    write cloudtrail log delivery stamp to metadata

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-31 20:10:50 UTC
+# Last sync: 2026-07-31 21:21:39 UTC
