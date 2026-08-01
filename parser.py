d0229d13c1610715fbc8c5de2a0ee6472edc4828
James Holloway <james.holloway94@gmail.com>
2026-08-01 08:56:13 +0000

    frame ssm document sync time in config record

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-01 07:55:02 UTC
+# Last sync: 2026-08-01 08:56:13 UTC
