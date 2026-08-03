b87cbbb63780bb7bbaaa7c64eb6975de77fcc8ec
James Holloway <james.holloway94@gmail.com>
2026-08-03 22:20:16 +0000

    stage rds connection pool sync for scheduled refresh

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-03 21:26:52 UTC
+# Last sync: 2026-08-03 22:20:16 UTC
