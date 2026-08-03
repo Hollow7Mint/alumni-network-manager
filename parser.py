1a24bd530e1754fdd786eda3a0aa08c7700c94f5
James Holloway <james.holloway94@gmail.com>
2026-08-03 20:58:16 +0000

    pin bastion ip allowlist time in config

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-03 19:33:44 UTC
+# Last sync: 2026-08-03 20:58:16 UTC
