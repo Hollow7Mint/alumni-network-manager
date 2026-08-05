9e70340c7771324095317e72a08bea4d4c3bc743
James Holloway <james.holloway94@gmail.com>
2026-08-05 21:16:34 +0000

    tag postgres checkpoint sync for replication trail

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-05 20:52:12 UTC
+# Last sync: 2026-08-05 21:16:34 UTC
