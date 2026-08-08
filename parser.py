5f1cb715f1730a174bb066be0018233b2a744030
James Holloway <james.holloway94@gmail.com>
2026-08-08 15:42:47 +0000

    annotate ec2 ami bake time for deploy record

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-08 14:14:32 UTC
+# Last sync: 2026-08-08 15:42:47 UTC
