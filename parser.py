5b6e3c568578a6f28f74b8411b699fa46ab8f31e
James Holloway <james.holloway94@gmail.com>
2026-08-08 18:35:36 +0000

    resolve waf rule group sync into metadata

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-08 17:10:54 UTC
+# Last sync: 2026-08-08 18:35:36 UTC
