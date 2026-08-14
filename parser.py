7d11cfaaf11a33bfc50603751369d26df4bdddb7
James Holloway <james.holloway94@gmail.com>
2026-08-14 19:53:18 +0000

    inspect iam trust policy sync marker for review

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-14 18:18:09 UTC
+# Last sync: 2026-08-14 19:53:17 UTC
