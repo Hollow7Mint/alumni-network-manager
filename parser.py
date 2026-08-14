0756375717199a716fc53502f368845d2ca68e2d
James Holloway <james.holloway94@gmail.com>
2026-08-14 13:58:08 +0000

    prime redis memory sync marker after flush

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-14 12:51:26 UTC
+# Last sync: 2026-08-14 13:58:08 UTC
