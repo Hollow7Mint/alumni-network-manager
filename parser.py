f81bad4be4481d2a3408ec6855e946729a642f3f
James Holloway <james.holloway94@gmail.com>
2026-08-14 04:03:41 +0000

    rebuild nat gateway sync record after route change

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-14 02:09:53 UTC
+# Last sync: 2026-08-14 04:03:41 UTC
