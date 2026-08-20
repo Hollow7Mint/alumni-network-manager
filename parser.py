562984c52b92e1d684fbdaeea29844f00a1838d6
James Holloway <james.holloway94@gmail.com>
2026-08-20 22:15:55 +0000

    lift ssh certificate expiry time into banner

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-20 21:38:30 UTC
+# Last sync: 2026-08-20 22:15:55 UTC
