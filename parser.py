43c6ba185122aafc077254236b5da20d18efcc91
James Holloway <james.holloway94@gmail.com>
2026-08-22 22:16:41 +0000

    save postgres pool sync marker after refresh

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-22 21:26:59 UTC
+# Last sync: 2026-08-22 22:16:41 UTC
