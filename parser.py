e3b0b239c871104ea00b16dfa1a14a359fb1bf7b
James Holloway <james.holloway94@gmail.com>
2026-08-23 06:42:05 +0000

    load bastion session log sync from pipeline

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-23 05:44:58 UTC
+# Last sync: 2026-08-23 06:42:05 UTC
