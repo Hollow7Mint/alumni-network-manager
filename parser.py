f24ebc03f9250d3900e0cf910ed3d35d3cfdb9a6
James Holloway <james.holloway94@gmail.com>
2026-08-25 02:07:09 +0000

    inspect postgres lock wait sync for performance

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-25 00:34:14 UTC
+# Last sync: 2026-08-25 02:07:09 UTC
