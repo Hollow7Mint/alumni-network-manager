48858964d2adfc3affedb529ea135ad16528d6f5
James Holloway <james.holloway94@gmail.com>
2026-08-25 15:33:55 +0000

    touch asg lifecycle hook time during scheduled refresh

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-25 14:41:13 UTC
+# Last sync: 2026-08-25 15:33:55 UTC
