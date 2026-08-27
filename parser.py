29a01619ce182469b6e160cb88ef5530e0ea9b4b
James Holloway <james.holloway94@gmail.com>
2026-08-27 06:24:31 +0000

    touch redis sentinel sync during scheduled refresh

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-27 05:41:39 UTC
+# Last sync: 2026-08-27 06:24:31 UTC
