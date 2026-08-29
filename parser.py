cc2c7b437785a89c2768259fe679e131d2529ed9
James Holloway <james.holloway94@gmail.com>
2026-08-29 09:37:32 +0000

    track guardduty threat archive in config heartbeat

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-29 08:26:24 UTC
+# Last sync: 2026-08-29 09:37:32 UTC
