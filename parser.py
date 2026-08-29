a22f8c743c6c6c98653dc0f9dcb83da5b801b0dc
James Holloway <james.holloway94@gmail.com>
2026-08-29 20:43:18 +0000

    log shield ddos event time in protection banner

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-29 19:41:57 UTC
+# Last sync: 2026-08-29 20:43:18 UTC
