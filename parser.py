58814c7863e2fc752bc28657f60f649f9decfe28
James Holloway <james.holloway94@gmail.com>
2026-09-01 17:19:59 +0000

    inspect bastion port forward sync for security

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-09-01 16:22:15 UTC
+# Last sync: 2026-09-01 17:19:59 UTC
