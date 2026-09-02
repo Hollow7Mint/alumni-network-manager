3720a9ab8e61cbfbe507fbe31d1ea863d4181cf5
James Holloway <james.holloway94@gmail.com>
2026-09-02 16:39:52 +0000

    index bastion user sync during audit pass

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-09-02 15:13:46 UTC
+# Last sync: 2026-09-02 16:39:52 UTC
