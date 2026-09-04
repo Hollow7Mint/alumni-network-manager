9d3043fe95038e4dfe68d43fb67d5a6e638625d2
James Holloway <james.holloway94@gmail.com>
2026-09-04 14:46:37 +0000

    ship ssh key sync record after nightly apply

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-09-04 13:45:13 UTC
+# Last sync: 2026-09-04 14:46:37 UTC
