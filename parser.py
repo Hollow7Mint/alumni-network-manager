8eab160d6f5b820fc9f46e75e88c95783c53e746
James Holloway <james.holloway94@gmail.com>
2026-09-04 23:08:37 +0000

    bump postgres schema sync stamp after migration

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-09-04 21:12:43 UTC
+# Last sync: 2026-09-04 23:08:37 UTC
