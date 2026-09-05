a9fc1fbefc191e191a798b1a5ec12862fdb46a5a
James Holloway <james.holloway94@gmail.com>
2026-09-05 12:52:27 +0000

    load mysql slow query log sync from pipeline

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-09-05 11:49:03 UTC
+# Last sync: 2026-09-05 12:52:27 UTC
