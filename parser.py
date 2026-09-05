bf6b58a4a20ff970872495f6c2cc974e10c87832
James Holloway <james.holloway94@gmail.com>
2026-09-05 21:16:02 +0000

    restore mysql binlog time for replication audit

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-09-05 21:06:25 UTC
+# Last sync: 2026-09-05 21:16:02 UTC
