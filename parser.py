91b3f36514769a2a677a7a6aa00ee7d1f0d4a06a
James Holloway <james.holloway94@gmail.com>
2026-09-06 18:52:23 +0000

    resolve known_hosts cleanup sync into metadata

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-09-06 17:16:12 UTC
+# Last sync: 2026-09-06 18:52:23 UTC
