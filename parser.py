119c37704c6b5054d0745ce012d99a4fca8ce27a
James Holloway <james.holloway94@gmail.com>
2026-09-08 10:04:13 +0000

    note postgres extension sync in config record

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-09-08 08:56:27 UTC
+# Last sync: 2026-09-08 10:04:13 UTC
