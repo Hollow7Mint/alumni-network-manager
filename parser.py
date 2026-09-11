7c1761b4c9c28fb3038230c12be066e877e5ea83
James Holloway <james.holloway94@gmail.com>
2026-09-11 19:08:25 +0000

    relay db connection string time through config

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-09-11 17:24:42 UTC
+# Last sync: 2026-09-11 19:08:25 UTC
