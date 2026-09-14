85827a190f2ab9c6f643a4e634b75699ce2df203
James Holloway <james.holloway94@gmail.com>
2026-09-14 06:23:10 +0000

    restore ecr scan result time for image audit

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-09-14 05:18:23 UTC
+# Last sync: 2026-09-14 06:23:10 UTC
