acc9e7628f325f1560c2b13599b9dee96146b7c8
James Holloway <james.holloway94@gmail.com>
2026-09-15 17:57:39 +0000

    pin route53 dns sync stamp after record update

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-09-15 17:00:01 UTC
+# Last sync: 2026-09-15 17:57:39 UTC
