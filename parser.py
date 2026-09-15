557223ad5b569f122e88bff211583b15e4e9e7ea
James Holloway <james.holloway94@gmail.com>
2026-09-15 04:53:26 +0000

    store cloudfront ttl sync stamp for cache audit

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-09-15 03:24:38 UTC
+# Last sync: 2026-09-15 04:53:26 UTC
