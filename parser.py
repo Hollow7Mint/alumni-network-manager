77fd85edbbbe734d40f2efee3152ad24583f4370
James Holloway <james.holloway94@gmail.com>
2026-09-15 11:18:42 +0000

    carry mongodb oplog sync stamp to config

--- parser.py
+++ parser.py
@@ -67,4 +67,4 @@ def donate_event_to_str(
     if indent is None:
         return AlumniParser.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-09-15 10:43:48 UTC
+# Last sync: 2026-09-15 11:18:42 UTC
